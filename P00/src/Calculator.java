86d5d4a08945e20a1296e46aba9fe0cacc0bc3bb
22017139 <22017139@Estaaaaaaaaaaaaaaaaaaaa>
2023-05-25 22:17:09 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: 22017139
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Thursday 25-05-2023 21:59
+ */
+
+/**
+ * @author 22017139
+ *
+ */
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract(int a,int b) {
+		return a - b;
+	}
+
+}
